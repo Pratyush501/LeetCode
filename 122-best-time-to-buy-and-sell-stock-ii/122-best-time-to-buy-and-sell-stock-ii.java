ed65ca1cad65cae76d3cbfbcc072e8257fe32724
Pratyush Prakash <80883776+Pratyush501@users.noreply.github.com>
2022-06-24 01:26:40 +0530

    Time: 1 ms (96.85%), Space: 42.1 MB (91.88%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,33 +1,40 @@
 class Solution {
-    public int maxProfit(int[] p) {
-        int n = p.length;
-        int temp[] = new int[n];
-        int t=0;
-        temp[n-1]=p[n-1];
-        Stack<Integer> st = new Stack<Integer>();
-        st.push(p[n-1]);
-        for(int i=n-2;i>=0;i--)
-        {
+    public int maxProfit(int[] prices) {
+//         int n = p.length;
+//         int temp[] = new int[n];
+//         int t=0;
+//         temp[n-1]=p[n-1];
+//         Stack<Integer> st = new Stack<Integer>();
+//         st.push(p[n-1]);
+//         for(int i=n-2;i>=0;i--)
+//         {
           
-            if(!st.isEmpty())
-            {
-                while(st.isEmpty()==false && p[i]>=st.peek())
-                    {
-                        st.pop();
-                    }
-            }
+//             if(!st.isEmpty())
+//             {
+//                 while(st.isEmpty()==false && p[i]>=st.peek())
+//                     {
+//                         st.pop();
+//                     }
+//             }
             
-            temp[i]=(st.isEmpty() ? p[i]: st.peek());
+//             temp[i]=(st.isEmpty() ? p[i]: st.peek());
             
-            st.push(p[i]);
-        }
-        int profit=0;
-        for(int i=0;i<n-1;i++)
-        {
-            if(p[i]<p[i+1])
-                profit=profit + temp[i]-p[i];
-        }
+//             st.push(p[i]);
+//         }
+//         int profit=0;
+//         for(int i=0;i<n-1;i++)
+//         {
+//             if(p[i]<p[i+1])
+//                 profit=profit + temp[i]-p[i];
+//         }
         
-        return profit;
+//         return profit;
+        
+         int n = prices.length;
+        int ans = 0;
+        for(int i=0;i<n-1;i++){
+            if(prices[i]<prices[i+1]) ans+=prices[i+1]-prices[i];
+        }
+        return ans;
     }
 }
